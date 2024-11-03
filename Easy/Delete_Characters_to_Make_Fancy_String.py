47e99b43adaec6e71f09d02861e2730e61106b8c
Tai <161024982+takoxtaion@users.noreply.github.com>
2024-11-03 13:21:54 +0400

    Update Delete_Characters_to_Make_Fancy_String.py

--- Easy/Delete_Characters_to_Make_Fancy_String.py
+++ Easy/Delete_Characters_to_Make_Fancy_String.py
@@ -1,21 +1,15 @@
-class Solution:
-    def makeFancyString(self, s: str) -> str:
-        result = []
-        count = 0
-
-        for i in s:
-            if result and result[-1] == i:
-                count += 1
-            else:
-                count = 1
-            if count <= 2:
-                result.append(i)
-
-        return "".join(result)
+def makeFancyString(s):
+    result = []
+    count = 0
+    for i in s:
+        if result and result[-1] == i:
+            count += 1
+        else:
+            count = 1
+        if count <= 2:
+            result.append(i)
+    return "".join(result)
 
 
 # Example:
-solution = Solution()
-s = "LeeetCode"
-result = solution.makeFancyString(s)
-print(result)
+print(makeFancyString("LeeetCode"))
