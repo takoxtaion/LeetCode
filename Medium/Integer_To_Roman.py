13f23bbed91436177929fc830bdc3c0795d117a7
Tai <161024982+takoxtaion@users.noreply.github.com>
2024-11-04 21:00:07 +0400

    Update and rename Integer_To_Roman.py to Medium/Integer_To_Roman.py

--- Medium/Integer_To_Roman.py
+++ Medium/Integer_To_Roman.py
@@ -1,27 +1,15 @@
 def intToRoman(num) -> str:
     val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     symbols = [
-        "M",
-        "CM",
-        "D",
-        "CD",
-        "C",
-        "XC",
-        "L",
-        "XL",
-        "X",
-        "IX",
-        "V",
-        "IV",
-        "I",
+        "M","CM","D","CD","C","XC","L",
+        "XL","X","IX","V","IV","I",
     ]
     output = ""
     for i in range(len(val)):
         while num >= val[i]:
             num -= val[i]
             output += symbols[i]
-
     return output
 
-
+#Example
 print(intToRoman(17))
